9a0025587dbd8646c5cbfa9593ce257c129f44a4
KevinMcblack <970017280@qq.com>
2019-03-29 13:07:31 +0800

    1.增加了 我的页面 1.1增加了 增加了个人信息页面 1.2增加了 增加了收藏页面 2.增加了 商城页面 3.增加了 设置页面

--- app/src/main/java/com/pro516/thrifttogether/ui/mall/MallFragment.java
+++ app/src/main/java/com/pro516/thrifttogether/ui/mall/MallFragment.java
@@ -1,7 +1,10 @@
 package com.pro516.thrifttogether.ui.mall;
 
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.pro516.thrifttogether.R;
 import com.pro516.thrifttogether.ui.base.BaseFragment;
@@ -11,6 +14,7 @@ import com.pro516.thrifttogether.ui.base.BaseFragment;
  */
 public class MallFragment extends BaseFragment {
 
+    private RecyclerView mRvGrid;
     @Override
     protected int getLayoutRes() {
         return R.layout.fragment_mall;
@@ -19,6 +23,17 @@ public class MallFragment extends BaseFragment {
     @Override
     protected void init(View view) {
         TextView mTitle = view.findViewById(R.id.title);
+        mRvGrid=view.findViewById(R.id.rv_grid);
         mTitle.setText(getString(R.string.points_mall));
+
+        mRvGrid.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        mRvGrid.setAdapter(new GridAdapter(getActivity(), new GridAdapter.OnItemClickListener()
+        {
+            @Override
+            public void onClick(int pos)
+            {
+                Toast.makeText(getActivity(), "click: " + pos, Toast.LENGTH_SHORT).show();
+            }
+        }));
     }
 }
--- app/src/main/java/com/pro516/thrifttogether/ui/mine/MineFragment.java
+++ app/src/main/java/com/pro516/thrifttogether/ui/mine/MineFragment.java
@@ -1,6 +1,8 @@
 package com.pro516.thrifttogether.ui.mine;
 
+import android.content.Intent;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.pro516.thrifttogether.R;
 import com.pro516.thrifttogether.ui.base.BaseFragment;
@@ -9,10 +11,34 @@ import com.pro516.thrifttogether.ui.base.BaseFragment;
  * Created by hncboy on 2019-03-19.
  */
 public class MineFragment extends BaseFragment {
+    private LinearLayout mine_setting;
 
     @Override
     protected void init(View view) {
+        mine_setting = view.findViewById(R.id.mine_setting);
+        setListeners();
+    }
 
+    private void setListeners() {
+        OnClick onClick = new OnClick();
+        mine_setting.setOnClickListener(onClick);
+
+    }
+
+    private class OnClick implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            Intent intent = null;
+            switch (view.getId()) {
+                case R.id.mine_setting:
+                    intent = new Intent(getActivity(), MineSetting.class);
+                    break;
+                default:
+                    break;
+            }
+            startActivity(intent);
+        }
     }
 
     @Override
